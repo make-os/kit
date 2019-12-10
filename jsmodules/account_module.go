6bb4404de93f142b7a00bc1aff9942b67bfbbac1
nedy <kennedyidialu@gmail.com>
2019-12-10 17:30:15 +0100

    Renamed to `setCommission`

--- jsmodules/account_module.go
+++ jsmodules/account_module.go
@@ -82,8 +82,8 @@ func (m *AccountModule) namespacedFuncs() []*types.JSModuleFunc {
 			Description: "Get the private validator information",
 		},
 		&types.JSModuleFunc{
-			Name:        "execSetDelegatorCommissionRate",
-			Value:       m.execSetDelegatorCommissionRate,
+			Name:        "setCommission",
+			Value:       m.setCommission,
 			Description: "Set the percentage of reward to share with a delegator",
 		},
 	}
@@ -242,8 +242,8 @@ func (m *AccountModule) getPrivateValidator(includePrivKey ...bool) interface{} 
 	return info
 }
 
-// execSetDelegatorCommissionRate sets the delegator commission for an account
-func (m *AccountModule) execSetDelegatorCommissionRate(txObj map[string]interface{},
+// setCommission sets the delegator commission for an account
+func (m *AccountModule) setCommission(txObj map[string]interface{},
 	options ...interface{}) interface{} {
 	var err error
 	tx, key := processTxArgs(txObj, options...)
