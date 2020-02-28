84687df974b57ff83f63c6f197252cbf2c83a56f
Kennedy <kennedyidialu@gmail.com>
2020-02-28 12:25:04 +0100

    Completed support for merge request command
    - Introduced specific error types `BadFieldError` and `StatusError`.
    - Added additional method to RPC and Rest clients.
    - Moved commandline entrypoints from `repo` into `repo/cmd`.

---
+++ api/rpc/transaction.go
@@ -0,0 +1,42 @@
+package rpc
+
+import (
+	"gitlab.com/makeos/mosdef/modules"
+	"gitlab.com/makeos/mosdef/rpc"
+	"gitlab.com/makeos/mosdef/types"
+	"gitlab.com/makeos/mosdef/util"
+)
+
+// TransactionAPI provides RPC methods for various local account management functionality.
+type TransactionAPI struct {
+	mods *modules.Modules
+}
+
+// NewTransactionAPI creates an instance of TransactionAPI
+func NewTransactionAPI(mods *modules.Modules) *TransactionAPI {
+	return &TransactionAPI{mods}
+}
+
+// sendPayload sends a signed transaction object to the mempool
+// Body <map>: Transaction object
+// Response <map>
+// - hash <string>: The transaction hash
+func (t *TransactionAPI) sendPayload(params interface{}) (resp *rpc.Response) {
+	txMap, ok := params.(map[string]interface{})
+	if !ok {
+		msg := util.WrongFieldValueMsg("params", "map", params).Error()
+		return rpc.Error(types.RPCErrCodeInvalidParamValue, msg, nil)
+	}
+	return rpc.Success(t.mods.Tx.SendPayload(txMap))
+}
+
+// APIs returns all API handlers
+func (l *TransactionAPI) APIs() rpc.APISet {
+	return map[string]rpc.APIInfo{
+		"sendPayload": {
+			Namespace:   types.NamespaceTx,
+			Description: "Sends a signed transaction object to the mempool",
+			Func:        l.sendPayload,
+		},
+	}
+}
