ea362eeb11dbb2906335db11ff23ae053a5f542d
Kennedy <kennedyidialu@gmail.com>
2020-03-19 11:30:35 +0100

    Added command for generating ed25519 keys

--- cmd/keys.go
+++ cmd/keys.go
@@ -15,12 +15,14 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	path "path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"gitlab.com/makeos/mosdef/config"
+	"gitlab.com/makeos/mosdef/crypto"
 	"gitlab.com/makeos/mosdef/keystore"
 	"gitlab.com/makeos/mosdef/types/core"
 )
@@ -29,8 +31,7 @@ import (
 var keysCmd = &cobra.Command{
 	Use:   "key command [flags]",
 	Short: "Create and manage your account and push keys.",
-	Long: `Description:
-This command provides the ability to create, list, import and update 
+	Long: `This command provides the ability to create, list, import and update 
 keys. Keys are stored in an encrypted format using a passphrase. 
 Please understand that if you forget the password, it is IMPOSSIBLE to 
 unlock your key. 
@@ -85,8 +86,7 @@ Always backup your keeps regularly.`,
 var keyListCmd = &cobra.Command{
 	Use:   "list [flags]",
 	Short: "List all accounts.",
-	Long: `Description:
-This command lists all accounts existing under <DATADIR>/` + config.KeystoreDirName + `.
+	Long: `This command lists all accounts existing under <DATADIR>/` + config.KeystoreDirName + `.
 
 Given that keys in the keystore directory are prefixed with their creation timestamp, the 
 list is lexicographically sorted such that the oldest keystore will be at the top on the list
@@ -102,8 +102,7 @@ list is lexicographically sorted such that the oldest keystore will be at the to
 var keyUpdateCmd = &cobra.Command{
 	Use:   "update [flags] <address>",
 	Short: "Update a key",
-	Long: `Description:
-This command allows you to update the password of a key and to
+	Long: `This command allows you to update the password of a key and to
 convert a key encrypted in an old format to a new one.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -116,7 +115,7 @@ convert a key encrypted in an old format to a new one.
 		pass, _ := cmd.Flags().GetString("pass")
 
 		ks := keystore.New(path.Join(cfg.DataDir(), config.KeystoreDirName))
-		if err := ks.UpdateCmd(address, pass); err != nil {
+		if err := ks.UpdateCmd(address, pass, os.Stdout); err != nil {
 			log.Fatal(err.Error())
 		}
 	},
@@ -125,8 +124,7 @@ convert a key encrypted in an old format to a new one.
 var keyImportCmd = &cobra.Command{
 	Use:   "import [flags] <keyfile>",
 	Short: "Import an existing, unencrypted private key.",
-	Long: `Description:
-This command allows you to create a new key by importing a private key from a <keyfile>. 
+	Long: `This command allows you to create a new key by importing a private key from a <keyfile>. 
 You will be prompted to provide your password. Your key is saved in an encrypted format.
 
 The keyfile is expected to contain an unencrypted private key in Base58 format.
@@ -161,8 +159,7 @@ key.
 var keyRevealCmd = &cobra.Command{
 	Use:   "reveal [flags] <address>",
 	Short: "Reveal the private key of a key.",
-	Long: `Description:
-This command reveals the private key of a key. You will be prompted to 
+	Long: `This command reveals the private key of a key. You will be prompted to 
 provide your password. 
 	
 You can skip the interactive mode by providing your password via the '--pass' flag. 
@@ -185,14 +182,42 @@ Also, the flag accepts a path to a file containing a password.
 	},
 }
 
+// keyGenCmd command generates ed25519 keys
+var keyGenCmd = &cobra.Command{
+	Use:   "gen [flags]",
+	Short: "Generate an ed25519 key",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		seed, _ := cmd.Flags().GetInt64("seed")
+
+		var key *crypto.Key
+		var err error
+		if seed != 0 {
+			key, err = crypto.NewKey(&seed)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else {
+			key, _ = crypto.NewKey(nil)
+		}
+
+		fmt.Println("Private Key:", key.PrivKey().Base58())
+		fmt.Println("Public Key: ", key.PubKey().Base58())
+		fmt.Println("Account Address: ", key.PubKey().Addr())
+		fmt.Println("Push Address: ", key.PubKey().PushAddr())
+	},
+}
+
 func setKeyCmdAndFlags() {
 	keysCmd.AddCommand(keyCreateCmd)
 	keysCmd.AddCommand(keyListCmd)
 	keysCmd.AddCommand(keyUpdateCmd)
 	keysCmd.AddCommand(keyImportCmd)
 	keysCmd.AddCommand(keyRevealCmd)
+	keysCmd.AddCommand(keyGenCmd)
 	keysCmd.PersistentFlags().String("pass", "", "Password to unlock the target key and skip interactive mode")
 	keyCreateCmd.Flags().Int64P("seed", "s", 0, "Provide a strong seed (not recommended)")
+	keyGenCmd.Flags().Int64P("seed", "s", 0, "Provide a strong seed (not recommended)")
 	keyCreateCmd.Flags().Bool("nopass", false, "Force key to be created with no passphrase")
 	keyCreateCmd.Flags().Bool("push", false, "Create a push key")
 	keyImportCmd.Flags().Bool("push", false, "Create a push key")
