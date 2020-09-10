680e79c2a7b336451b746530609ed1ed214bc23e
Kennedy <kennedyidialu@gmail.com>
2020-09-10 15:53:21 +0100

    Added testnet seed addresses

--- config/config.go
+++ config/config.go
@@ -58,15 +58,18 @@ var (
 	// PersistentSeedPeers are peers are trusted, permanent peers to connect us to the network.
 	// They will be redialed on connection failure.
 	PersistentSeedPeers = []string{
-		"aba9f171986276b6a0f43f89cae96941f77819b6@s1.seeders.live:9000",
-		"aba9f171986276b6a0f43f89cae96941f77819b6@s2.seeders.live:9000",
-		"aba9f171986276b6a0f43f89cae96941f77819b6@s3.seeders.live:9000",
-		"aba9f171986276b6a0f43f89cae96941f77819b6@s4.seeders.live:9000",
+		"a2f1e5786d3564c14faafffd6a050d2f81c655d9@s1.seeders.live:9000",
+		"9cd75740de0c9d7b2a5d3921b78abbbb39b1bebe@s2.seeders.live:9000",
+		"3ccd79a6f332f83b85f63290ca53187022aada0a@s3.seeders.live:9000",
+		"d0165f00485e22ec0197e15a836ce66587515a84@s4.seeders.live:9000",
 	}
 
 	// SeedDHTPeers are DHT seed peers to connect to.
 	SeedDHTPeers = []string{
-		"/ip4/127.0.0.1/tcp/9003/p2p/12D3KooWHo3qkVVjErFR4tkFGdn4exeD2nbfidQFYFPfN2usVzim",
+		"/ip4/s1.seeders.live/tcp/9003/p2p/12D3KooWAeorTJTi3uRDC3nSMa1V9CujJQg5XcN3UjSSV2HDceQU",
+		"/ip4/s2.seeders.live/tcp/9003/p2p/12D3KooWEksv3Nvbv5dRwKRkLJjoLvsuC6hyokj5sERx8mWrxMoB",
+		"/ip4/s3.seeders.live/tcp/9003/p2p/12D3KooWJzM4Hf5KWrXnAJjgJkro7zK2edtDu8ocYt8UgU7vsmFa",
+		"/ip4/s4.seeders.live/tcp/9003/p2p/12D3KooWE7KybnAaoxuw6UiMpof2LT9hMky8k83tZgpdNCqRWx9P",
 	}
 )
 
